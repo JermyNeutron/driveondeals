21378ac848ea1d91237f68b5249f004045e80120
Jerms-macos <jerms@Jermss-iMac.local>
2024-09-24 08:54:47 -0700

    added a test function

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ import time #removable
 from test import test_alamo
 
 
+# Random access function
+def access_test1(test):
+    if test:
+        return_time = test_alamo.find_time()
+    print(return_time)
+
+
 def run_alamo(test):
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=True)
@@ -16,3 +23,4 @@ def run_alamo(test):
 if __name__ == "__main__":
     test = True
     run_alamo(test)
+    # access_test1(test)
