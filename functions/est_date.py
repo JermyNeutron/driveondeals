048db11840429a1e1c8888600a64ea9c40e422e3
Jeremy Desktop <jeremycrisostomo@gmail.com>
2024-10-11 14:33:41 -0700

    updated function to include datetime's %B for YYYY-MM-DD return value

--- functions/est_date.py
+++ functions/est_date.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 
 # Standard current time retrieval
-def get_now(test: bool, hints_enabled: bool) -> tuple[str, str, str, str, str]:
+def get_now(test: bool, hints_enabled: bool) -> tuple[str, str, str, str, str, str, str]:
     """
     Automatically retrieves the current date and time.
 
@@ -14,12 +14,13 @@ def get_now(test: bool, hints_enabled: bool) -> tuple[str, str, str, str, str]:
     Returns:
         tuple: a tuple containing:
             0) current_time (str): The current time in format HH:MM:SS, e.g. "07:25:40".
-            1) date_current_day (str): The current day (DD).
-            2) date_current_day_sfx (str): (DD) with suffix, e.g. 27th.
-            3) date_dow (str): "Day of Week", e.g. Tuesday.
-            4) date_current_month_ind (str): The current month (MM).
-            5) date_current_month_str (str): The current month, e.g. September.
-            6) date_current_year (str): The current year (YYYY).
+            1) date_current (str): The current date (YYYY-MM-DD).
+            2) date_current_day (str): The current day (DD).
+            3) date_current_day_sfx (str): (DD) with suffix, e.g. 27th.
+            4) date_dow (str): "Day of Week", e.g. Tuesday.
+            5) date_current_month_ind (str): The current month (MM).
+            6) date_current_month_str (str): The current month, e.g. September.
+            7) date_current_year (str): The current year (YYYY).
     """
 
 
@@ -60,6 +61,7 @@ def get_now(test: bool, hints_enabled: bool) -> tuple[str, str, str, str, str]:
     
 
     time_current = time.strftime("%H:%M:%S")
+    date_current = datetime.now().strftime("%Y-%m-%d")
     date_current_day = str(int(time.strftime("%d"))) # (str)
     date_current_day_sfx = find_suffix(test, hints_enabled, date_current_day) # (str)
     date_dow = datetime.now().strftime("%A")
@@ -68,13 +70,14 @@ def get_now(test: bool, hints_enabled: bool) -> tuple[str, str, str, str, str]:
     date_current_year = time.strftime("%Y")
     if hints_enabled:
         print(f"\nHINT {get_now}: {{time_current}}: {time_current}")
+        print(f"HINT {get_now}: {{date_current}}: {date_current}")
         print(f"HINT {get_now}: {{date_current_day}}: {date_current_day}")
         print(f"HINT {get_now}: {{date_current_day_sfx}}: {date_current_day_sfx}")
         print(f"HINT {get_now}: {{date_dow}}: {date_dow}")
         print(f"HINT {get_now}: {{date_current_month_ind}}: {date_current_month_ind}")
         print(f"HINT {get_now}: {{date_current_month_str}}: {date_current_month_str}")
         print(f"HINT {get_now}: {{date_current_year}}: {date_current_year}\n")
-    return time_current, date_current_day, date_current_day_sfx, date_dow, date_current_month_ind, date_current_month_str, date_current_year
+    return time_current, date_current, date_current_day, date_current_day_sfx, date_dow, date_current_month_ind, date_current_month_str, date_current_year
 
 
 # TESTS
@@ -84,7 +87,7 @@ def test_get_now(test: bool, hints_enabled: bool):
     """
     Test function to print return tuple.
 
-    "Automatically retrieves the current date and time."
+    Automatically retrieves the current date and time.
 
     Args:
         test (bool)
@@ -93,12 +96,13 @@ def test_get_now(test: bool, hints_enabled: bool):
     Returns:
         tuple: a tuple containing:
             0) current_time (str): The current time in format HH:MM:SS, e.g. "07:25:40".
-            1) date_current_day (str): The current day (DD).
-            2) date_current_day_sfx (str): (DD) with suffix, e.g. 27th.
-            3) date_dow (str): "Day of Week", e.g. Tuesday.
-            4) date_current_month_ind (str): The current month (MM).
-            5) date_current_month_str (str): The current month, e.g. September.
-            6) date_current_year (str): The current year (YYYY).
+            1) date_current (str): The current date (YYYY-MM-DD).
+            2) date_current_day (str): The current day (DD).
+            3) date_current_day_sfx (str): (DD) with suffix, e.g. 27th.
+            4) date_dow (str): "Day of Week", e.g. Tuesday.
+            5) date_current_month_ind (str): The current month (MM).
+            6) date_current_month_str (str): The current month, e.g. September.
+            7) date_current_year (str): The current year (YYYY).
     """
     meta_date_today = get_now(test, hints_enabled)
     for i, value in enumerate(meta_date_today, start=0):
@@ -107,6 +111,6 @@ def test_get_now(test: bool, hints_enabled: bool):
 
 if __name__ == "__main__":
     test = True
-    hints_enabled = True
+    hints_enabled = False
     # meta_date_today = get_now(test, hints_enabled) # important for meta to be collected upon initial program execution
     test_get_now(test, hints_enabled)
