093b450fbd709b9c76281c95dfa4491005699827
Jeremy Desktop <jeremycrisostomo@gmail.com>
2024-10-12 17:47:04 -0700

    updated aria_label with new return values from est_date.get_now()

--- main.py
+++ main.py
@@ -7,11 +7,21 @@ from test import test_alamo
 from functions import est_date
 
 
-# Random access function
-def access_test1(test):
-    if test:
-        return_time = test_alamo.find_time()
-    print(return_time)
+# # Random access function
+# def access_test1(test):
+#     if test:
+#         return_time = test_alamo.find_time()
+#     print(return_time)
+
+
+def get_now(test, hints_enabled):
+    # Determine today's date
+    meta_date_today = est_date.get_now(test, hints_enabled)
+    # Aria Label format:
+    # "Choose Saturday, October 12th, 2024"
+    aria_label = f"Choose {meta_date_today[4]}, {meta_date_today[6]} {meta_date_today[3]}, {meta_date_today[7]}"
+    hints_enabled and print(f"HINT {get_now} aria_label: {aria_label}") # removable
+    return meta_date_today, aria_label
 
 
 def run_alamo(test: bool, hints_enabled: bool, hl_mode: bool, get_now_aria_label: str):
@@ -20,14 +30,6 @@ def run_alamo(test: bool, hints_enabled: bool, hl_mode: bool, get_now_aria_label
         page = browser.new_page()
         test_alamo.test_basic_search(test, hints_enabled, get_now_aria_label, page) # change
         browser.close()
-
-
-def get_now(test, hints_enabled):
-    # Determine today's date
-    meta_date_today = est_date.get_now(test, hints_enabled)
-    aria_label = f"Choose {meta_date_today[3]}, {meta_date_today[5]} {meta_date_today[2]}, {meta_date_today[6]}"
-    hints_enabled and print(f"HINT {get_now} aria_label: {aria_label}") # removable
-    return meta_date_today, aria_label
 
 
 if __name__ == "__main__":
