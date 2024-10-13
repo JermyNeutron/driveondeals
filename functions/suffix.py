5cf894fce174f54feb85a6d0cbe898a0dce2a06f
Jeremy Desktop <jeremycrisostomo@gmail.com>
2024-10-12 21:04:20 -0700

    updated print statements

--- functions/suffix.py
+++ functions/suffix.py
@@ -14,7 +14,7 @@ def main(test: bool, hints_enabled: bool, date_day: str) -> str:
     """
     mod_day = date_day
     if int(date_day) < 1 or int(date_day) > 31:
-        hints_enabled and print("invalid day entered.")
+        hints_enabled and print(f"HINT {__name__}: invalid day entered.")
         pass # possible change return value
     else:
         if date_day in ("11", "12", "13"):
@@ -27,7 +27,7 @@ def main(test: bool, hints_enabled: bool, date_day: str) -> str:
             mod_day += "rd"
         else:
             mod_day += "th"
-        hints_enabled and print(f"HINT {__name__}: {date_day} returned as {mod_day}")
+        hints_enabled and print(f"HINT {__name__}: {date_day} returned as {mod_day}\n")
         return mod_day
         
 if __name__ == "__main__":
