e71cc205b4de097326b2b73cf2a91abdf71ec161
Jeremy Desktop <jeremycrisostomo@gmail.com>
2024-10-12 20:35:12 -0700

    updated comments; reduced datetime calls, date calls, and time calls; removed internal find_suffix() function to use imported suffix function

--- functions/est_date.py
+++ functions/est_date.py
@@ -1,9 +1,11 @@
-import sys
-
-sys.path.append("..")
-
 import time
 from datetime import datetime
+
+import sys
+
+sys.path.append(".")
+
+from functions import suffix
 
 
 # Standard current time retrieval
@@ -17,47 +19,16 @@ def get_now(test: bool, hints_enabled: bool) -> tuple[str, str, str, str, str, s
 
     Returns:
         tuple: a tuple containing:
-            0) current_time (str): The current time in format HH:MM:SS, e.g. "07:25:40".
-            1) date_current (str): The current date (YYYY-MM-DD).
-            2) date_current_day (str): The current day (DD).
-            3) date_current_day_sfx (str): (DD) with suffix, e.g. 27th.
-            4) date_dow (str): "Day of Week", e.g. Tuesday.
-            5) date_current_month_ind (str): The current month (MM).
-            6) date_current_month_str (str): The current month, e.g. September.
-            7) date_current_year (str): The current year (YYYY).
+            0) meta_krono (tup): Everything retrieved from datetime.now()
+            1) current_time (str): The current time in format HH:MM:SS, e.g. "07:25:40".
+            2) date_current (str): The current date (YYYY-MM-DD).
+            3) date_current_day (str): The current day (DD).
+            4) date_current_day_sfx (str): (DD) with suffix, e.g. 27th.
+            5) date_dow (str): "Day of Week", e.g. Tuesday.
+            6) date_current_month_ind (str): The current month (MM).
+            7) date_current_month_str (str): The current month, e.g. September.
+            8) date_current_year (str): The current year (YYYY).
     """
-
-
-    # Suffix function only tested on Alamo website
-    def find_suffix(test: bool, hints_enabled: bool, date_day: str) -> str:
-        """
-        Determines proper suffix for day selected as required in Alamo's website.
-
-        Args:
-            test (bool)
-            hints_enabled (bool)
-            date_day (str)
-
-        Returns:
-            mod_day (str): e.g. "23rd"
-        """
-        mod_day = date_day
-        if int(date_day) < 1 or int(date_day) > 31:
-            hints_enabled and print("invalid day entered.")
-            pass # possible change return value
-        else:
-            if date_day == "12":
-                mod_day += "th"
-            elif date_day[-1] == "1":
-                mod_day += "st"
-            elif date_day[-1] == "2":
-                mod_day += "nd"
-            elif date_day[-1] == "3":
-                mod_day += "rd"
-            else:
-                mod_day += "th"
-            hints_enabled and print(f"HINT {find_suffix}: {date_day} returned as {mod_day}")
-            return mod_day
         
 
     def minimum_date_selection(test: bool, hints_enabled: bool) -> str:
@@ -65,30 +36,30 @@ def get_now(test: bool, hints_enabled: bool) -> tuple[str, str, str, str, str, s
         # possible function to restrict backward reseverations
         pass
     
-
-    time_current = time.strftime("%H:%M:%S")
-    date_current = datetime.now().strftime("%Y-%m-%d")
-    date_current_day = str(int(time.strftime("%d"))) # (str)
-    date_current_day_sfx = find_suffix(test, hints_enabled, date_current_day) # (str)
-    date_dow = datetime.now().strftime("%A")
-    date_current_month_ind = time.strftime("%m")
-    date_current_month_str = datetime.now().strftime("%B")
-    date_current_year = time.strftime("%Y")
+    # POSSIBLE condense .strftime()'s to minimize tuple
+    meta_krono = datetime.now()
+    time_current = meta_krono.strftime("%H:%M:%S")
+    date_current = meta_krono.strftime("%Y-%m-%d")
+    date_current_day = meta_krono.strftime("%d")
+    date_current_day_sfx = suffix.main(test, hints_enabled, date_current_day) # (str)
+    date_dow = meta_krono.strftime("%A")
+    date_current_month_int = meta_krono.strftime("%m")
+    date_current_month_str = meta_krono.strftime("%B")
+    date_current_year = meta_krono.strftime("%Y")
     if hints_enabled:
-        print(f"\nHINT {get_now}: {{time_current}}: {time_current}")
-        print(f"HINT {get_now}: {{date_current}}: {date_current}")
-        print(f"HINT {get_now}: {{date_current_day}}: {date_current_day}")
-        print(f"HINT {get_now}: {{date_current_day_sfx}}: {date_current_day_sfx}")
-        print(f"HINT {get_now}: {{date_dow}}: {date_dow}")
-        print(f"HINT {get_now}: {{date_current_month_ind}}: {date_current_month_ind}")
-        print(f"HINT {get_now}: {{date_current_month_str}}: {date_current_month_str}")
-        print(f"HINT {get_now}: {{date_current_year}}: {date_current_year}\n")
-    return time_current, date_current, date_current_day, date_current_day_sfx, date_dow, date_current_month_ind, date_current_month_str, date_current_year
+        print(f"\nHINT {__name__}: {{meta_krono}}: {meta_krono}")
+        print(f"HINT {__name__}: {{time_current}}: {time_current}")
+        print(f"HINT {__name__}: {{date_current}}: {date_current}")
+        print(f"HINT {__name__}: {{date_current_day}}: {date_current_day}")
+        print(f"HINT {__name__}: {{date_current_day_sfx}}: {date_current_day_sfx}")
+        print(f"HINT {__name__}: {{date_dow}}: {date_dow}")
+        print(f"HINT {__name__}: {{date_current_month_int}}: {date_current_month_int}")
+        print(f"HINT {__name__}: {{date_current_month_str}}: {date_current_month_str}")
+        print(f"HINT {__name__}: {{date_current_year}}: {date_current_year}\n")
+    return meta_krono, time_current, date_current, date_current_day, date_current_day_sfx, date_dow, date_current_month_int, date_current_month_str, date_current_year
 
 
 # TESTS
-
-
 def test_get_now(test: bool, hints_enabled: bool):
     """
     Test function to print return tuple.
@@ -117,6 +88,6 @@ def test_get_now(test: bool, hints_enabled: bool):
 
 if __name__ == "__main__":
     test = True
-    hints_enabled = False
-    # meta_date_today = get_now(test, hints_enabled) # important for meta to be collected upon initial program execution
-    test_get_now(test, hints_enabled)
+    hints_enabled = True
+    
+    get_now(test, hints_enabled)
