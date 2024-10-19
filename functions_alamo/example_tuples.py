bffa73a37c0c07b3e252298974d0149ec6322813
Jerms-macos <jeremycrisostomo@gmail.com>
2024-10-19 02:27:15 -0700

    added scrape tuple array

---
+++ functions_alamo/example_tuples.py
@@ -0,0 +1,32 @@
+example_tuples = [('Midsize SUV', 'Nissan Rogue or similar', '5', '4', 'IFAR', '70.00', '88.30'),
+('Compact SUV', 'Hyundai Kona or similar', '5', '3', 'CFAR', '64.00', '80.38'),
+('Standard Pickup', 'Toyota Tacoma or similar', '4', '3', 'SPAR', '70.00', '89.58'),
+('Standard SUV', 'Chevrolet Equinox or similar', '5', '5', 'SFAR', '75.00', '94.79'),
+('Pickup', 'Ford F150 or similar', '4', '4', 'PPAR', '75.00', '96.12'),
+('You Click We Pick', 'Vehicle determined upon pick-up, Compact or Larger', None, None, 'XXAR', '77.03', '97.10'),
+('Economy', 'Mitsubishi Mirage or similar', '4', '2', 'ECAR', '81.97', '102.05'),
+('Compact', 'Nissan Versa or similar', '5', '2', 'CCAR', '81.89', '102.05'),
+('Midsize', 'Toyota Corolla or similar', '5', '3', 'ICAR', '83.65', '104.63'),
+('Premium Crossover', 'Dodge Durango or similar', '7', '3', 'PGAR', '84.00', '106.07'),
+('Standard', 'VW Jetta or similar', '5', '3', 'SCAR', '85.64', '107.03'),
+('Full Size', 'Toyota Camry or Chevrolet Malibu or similar', '5', '4', 'FCAR', '85.46', '107.68'),
+('Luxury', 'Audi A4 or BMW 3 Series or similar', '5', '4', 'LCAR', '88.53', '111.27'),
+('Premium', 'Nissan Maxima or similar', '5', '4', 'PCAR', '95.34', '119.57'),
+('Full Size Elite', 'Audi A5 Sportback or similar', '5', '3', 'GXAR', '97.63', '123.16'),
+('Premium Special', 'Audi A3 or BMW 2 Series Gran Coupe or similar', '5', '3', 'PXAR', '100.42', '125.54'),
+('Standard Elite', 'BMW 228i Gran Coupe or similar', '5', '2', 'RXAR', '100.10', '125.54'),
+('Standard Elite SUV', 'Audi Q3, Cadillac XT4 or similar', '5', '3', 'RFAR', '100.07', '125.54'),
+('Jeep', 'Jeep Wrangler 2 Door or similar', '4', '3', 'IJAR', '109.23', '136.77'),
+('Full Size SUV', 'Chevrolet Tahoe, Ford Expedition, Nissan Armada or similar', '7', '3', 'FFAR', '109.00', '137.17'),
+('7 Passenger Minivan', 'Chrysler Pacifica or similar', '7', '5', 'MVAR', '111.70', '139.92'),
+('Convertible', 'Ford Mustang Convertible or similar', '4', '2', 'STAR', '114.70', '143.70'),
+('Jeep Wrangler 4 door', 'Jeep Wrangler Unlimited or similar', '5', '5', 'FJAR', '114.98', '144.25'),
+('Luxury SUV', 'Lincoln Navigator, Infiniti QX80 or similar', '7', '3', 'LFAR', '166.29', '208.45'),
+('Premium', 'Chrysler 300 or similar', '5', '4', 'PDAR', '259.98', '322.15'),
+('Midsize Luxury SUV', 'Mercedes GLE, BMW X5 or similar', '5', '4', 'UDAR', '299.00', '371.87'),
+('Midsize Elite Luxury SUV', 'Audi Q7, Volvo XC90 or similar', '7', '4', 'WDAR', '299.00', '371.87'),
+('Full Size Luxury SUV', 'BMW X7, Range Rover Sport or similar', '5', '4', 'WFAR', '299.00', '371.87')]
+
+
+if __name__ == "__main__":
+    pass
