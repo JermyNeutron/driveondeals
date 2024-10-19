7d85480b8b9185ee0fa6838aba7eda6740582f59
Jerms-macos <jeremycrisostomo@gmail.com>
2024-10-19 04:13:13 -0700

    added docnotes and allowed the function to take custom filepaths

--- functions_gen/import_logging.py
+++ functions_gen/import_logging.py
@@ -1,11 +1,21 @@
 import logging
 
-def set_logging():
+
+def set_logging(filepath: str) -> None:
+    """
+    Sets basicConfig at debug level.
+
+    Args:
+        filepath (str)
+
+    Returns:
+        None
+    """
     logging.basicConfig(
         level=logging.DEBUG,
         format='%(asctime)s - %(levelname)s - %(message)s',
         handlers=[
-            logging.FileHandler("functions_alamo/alamo_dtm_logs.txt"),
+            logging.FileHandler(f"{filepath}"),
             logging.StreamHandler()
         ]
     )
