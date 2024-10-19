f193604f914ac9d1b976a38c077fd3b2635d9037
Jeremy Desktop <jeremycrisostomo@gmail.com>
2024-10-18 20:53:22 -0700

    created dictionary to track and update ['dtm']s when able

---
+++ functions_alamo/alamo_dtm.py
@@ -0,0 +1,106 @@
+dtm_list = {
+    # Sedans
+    "Compact": {"dtm": "CCAR", "description": None},
+    "Compact Convertible": {"dtm": None, "description": None}, 
+    "Compact Special": {"dtm": None, "description": None},
+    "Economy": {"dtm": "ECAR", "description": None},
+    "Full Size Hybrid": {"dtm": None, "description": None},
+    "Full Size": {"dtm": "FCAR", "description": None},
+    "Full Size Elite Electric": {"dtm": None, "description": None},
+    "Full Size Elite": {"dtm": "GXAR", "description": None},
+    "Intermediate Electric": {"dtm": None, "description": None},
+    "Midsize": {"dtm": "ICAR", "description": None},
+    "Midsize Convertible": {"dtm": None, "description": None},
+    "Luxury": {"dtm": "LCAR", "description": None},
+    "Luxury Sport": {"dtm": None, "description": None},
+    "Luxury Convertible": {"dtm": None, "description": None},
+    "Premium": {"dtm": "PCAR", "description": "Nissan Maxima or similar"},
+    "Premium": {"dtm": "PDAR", "description": "Chrysler 300 or similar"},
+    "American Muscle Car": {"dtm": None, "description": None}, 
+    "Premium Special": {"dtm": "PXAR", "description": None},
+    "Standard Elite": {"dtm": "RXAR", "description": None},
+    "Standard Hybrid": {"dtm": None, "description": None},
+    "Standard": {"dtm": "SCAR", "description": None},
+    "Sporty Car": {"dtm": None, "description": None},
+    "Convertible": {"dtm": "STAR", "description": None},
+    "Corvette": {"dtm": None, "description": None}, 
+    "Midsize Luxury Sedan": {"dtm": None, "description": None}, 
+    "Midsize Sport Luxury Sedan": {"dtm": None, "description": None}, 
+    "Electric Luxury Sedan": {"dtm": None, "description": None}, 
+    "Full Size Luxury Sedan": {"dtm": None, "description": None}, 
+    "Performance Sport": {"dtm": None, "description": None}, 
+    "Luxury Performance Sport": {"dtm": None, "description": None}, 
+    "Premium Luxury Sedan": {"dtm": None, "description": None}, 
+    "Ultra Performance Sport": {"dtm": None, "description": None}, 
+    "Elite Performance Sport": {"dtm": None, "description": None}, 
+    "Ultra Luxury Sedan": {"dtm": None, "description": None},
+
+    # SUVs
+    "Compact Electric SUV": {"dtm": None, "description": None},
+    "Compact SUV": {"dtm": "CFAR", "description": None},
+    "Compact SUV AWD": {"dtm": None, "description": None},
+    "Full Size SUV":  {"dtm": "FFAR", "description": "Chevrolet Tahoe, Ford Expedition, Nissan Armada or similar"}, # Chevrolet Taho, Ford Expedition, Nissan Armada or similar
+    "Full Size SUV AWD": {"dtm": None, "description": None},
+    "Jeep Wrangler 4 door": {"dtm": "FJAR", "description": None},
+    "Full Size SUV": {"dtm": None, "description": "Chevrolet Tahoe or similar"}, # Chevrolet Tahoe or similar
+    "Midsize SUV": {"dtm": "IFAR", "description": None},
+    "Midsize SUV AWD": {"dtm": None, "description": None},
+    "Jeep": {"dtm": "IJAR", "description": None},
+    "Luxury SUV": {"dtm": "LFAR", "description": None},
+    "Luxury SUV AWD": {"dtm": None, "description": None},
+    "Extended Luxury SUV": {"dtm": None, "description": None},
+    "Premium SUV": {"dtm": "PFAR", "description": None},
+    "Premium SUV AWD": {"dtm": None, "description": None},
+    "Premium Crossover": {"dtm": "PGAR", "description": None},
+    "Premium Crossover AWD": {"dtm": None, "description": None},
+    "Premium All-Terrain": {"dtm": None, "description": None},
+    "Standard Elite SUV": {"dtm": "RFAR", "description": None},
+    "Standard SUV": {"dtm": "SFAR", "description": None},
+    "Standard SUV AWD": {"dtm": None, "description": None},
+    "Standard Crossover": {"dtm": None, "description": None},
+    "Standard Crossover AWD": {"dtm": None, "description": None},
+    "Midsize Luxury SUV": {"dtm": "UDAR", "description": None},
+    "Premium Elite SUV": {"dtm": "UFAR", "description": None},
+    "Midsize Elite Luxury SUV": {"dtm": "WDAR", "description": None},
+    "Electric Luxury SUV": {"dtm": None, "description": None},
+    "Full Size Luxury SUV": {"dtm": None, "description": None},
+    "Special SUV": {"dtm": None, "description": None},
+    "Premium Luxury SUV": {"dtm": None, "description": None},
+    "Ultra Luxury SUV": {"dtm": None, "description": None},
+    "Medium Hybrid SUV": {"dtm": None, "description": None},
+
+    # Trucks
+    "Full Size Electric Pickup": {"dtm": None, "description": None},
+    "Pickup": {"dtm": "PPAR", "description": None},
+    "Full Size Diesel Pickup": {"dtm": None, "description": None},
+    "Full Size Electric Pickup 4WD": {"dtm": None, "description": None},
+    "Premium Pickup 4WD": {"dtm": None, "description": None},
+    "Standard Pickup": {"dtm": "SPAR", "description": None},
+    "Standard Pickup 4WD": {"dtm": None, "description": None},
+
+    # Vans
+    "15 Passenger Van": {"dtm": None, "description": None},
+    "10 Passenger Van": {"dtm": None, "description": None},
+    "7 Passenger Minivan": {"dtm": "MVAR", "description": None},
+    "12 Passenger Van Med/High Roof": {"dtm": None, "description": None},
+    "Heavy Duty Cargo Van 4WD": {"dtm": None, "description": None},
+    "12 Passenger Van": {"dtm": None, "description": None},
+    "Cargo Van": {"dtm": None, "description": None},
+    "Cargo Van 4WD": {"dtm": None, "description": None},
+    "8 Passenger Minivan": {"dtm": "SVAR", "description": None},
+    "Heavy Duty High Roof Cargo Van 4WD": {"dtm": None, "description": None},
+    "15 Passenger Van Med/High Roof": {"dtm": None, "description": None},
+    "Limo Van": {"dtm": None, "description": None},
+
+    # Special
+    "You Click We Pick": {"dtm": "XXAR", "description": "Vehicle determined upon pick-up, Compact or Large"},
+    "Drive Happy Default": {"dtm": None, "description": None},
+}
+
+
+def dtm_update(test: bool, hints_enabled: bool):
+    pass
+
+
+if __name__ == "__main__":
+    pass
