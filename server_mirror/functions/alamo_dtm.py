b6b714253d83b7b98611b7d276eedaddeb997db5
Jeremy Desktop <jeremycrisostomo@gmail.com>
2024-11-07 14:56:37 -0800

    cleaned up code and updated with improved logging call

--- server_mirror/functions/alamo_dtm.py
+++ server_mirror/functions/alamo_dtm.py
@@ -1,19 +1,17 @@
 # Server-Mirror
 
-import sys
 import json
-import logging
 
-import import_logging
-from resources import alamo_example_tuples
+from functions import import_logging
 
-import_logging.main("resources/alamo_dtm_logs.txt")
+dtm_logger = import_logging.main("../resources/alamo_dtm_logs.txt", "dtm_logger")
+
 
 def dtm_update(test: bool, hints_enabled: bool, query: list) -> None:
     """
     Compares and updates alamo_dtm.json with alamo classes.
 
-    Args:
+    Parameters:
         test (bool)
         hints_enabled (bool)
         query (list)
@@ -23,10 +21,10 @@ def dtm_update(test: bool, hints_enabled: bool, query: list) -> None:
     """
 
     # Load alamo dtm's
-    with open("resources/alamo_dtm.json", "r") as file:
+    with open("../resources/alamo_dtm.json", "r") as file:
         dtm_import_list = json.load(file)
 
-    # Was there a change?
+    # Counter for any changes made for json file.
     alt = 0
     # Compare dtms and descriptions
     for i in query:
@@ -38,25 +36,32 @@ def dtm_update(test: bool, hints_enabled: bool, query: list) -> None:
             test and print(dtm_import_list[i[2]])
             if i[3] != dtm_import_list[i[2]]["description"]:
                 dtm_import_list[i[2]]["description"] = i[3]
-                logging.info(f'HINT {__name__}: "{i[2]}" had their description assigned to "{i[3]}".')
+                dtm_logger.info(f'HINT {__name__}: "{i[2]}" had their description assigned to "{i[3]}".')
                 alt += 1
             if i[6] != dtm_import_list[i[2]]["dtm"]:
                 dtm_import_list[i[2]]["dtm"] = i[6]
-                logging.info(f'HINT {__name__}: "{i[2]}" had their dtm assigned to "{i[6]}".')
+                dtm_logger.info(f'HINT {__name__}: "{i[2]}" had their dtm assigned to "{i[6]}".')
                 alt += 1
         else:
             print(f'we found something new: {i[2]}')
-    
+
     if alt == 0:
         hints_enabled and print(f"HINT {__name__}: dtm's in alamo_dtm.json are up to date.")
     # write to json file
     else:
-        with open("resources/alamo_dtm.json", "w") as f:
-            json.dump(dtm_import_list, f, indent=4)        
+        with open("../resources/alamo_dtm.json", "w") as f:
+            json.dump(dtm_import_list, f, indent=4)
+
+
+def logging_test():
+    """Tests logging output to '../resources/alamo_dtm_logs.txt'"""
+    dtm_logger.info(f"{__name__}: dtm_logger made from alamo_dtm")
 
 
 if __name__ == "__main__":
     test = False
     hints_enabled = True
-    myvar = alamo_example_tuples.example_tuples
-    dtm_update(test, hints_enabled, myvar)
+
+    logging_test()
+
+    # dtm_update(test, hints_enabled, myvar)
