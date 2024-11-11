fb6b284e49ed0219c6999f08f8c5553483ba5c8a
Jeremy Desktop <jeremycrisostomo@gmail.com>
2024-11-11 01:38:24 -0800

    updated json structure with vehicle categories and updated dtm update function

--- functions_alamo/alamo_dtm.py
+++ functions_alamo/alamo_dtm.py
@@ -112,13 +112,13 @@ dtm_list = {
 }
 
 
-# Test with ('Midsize SUV', 'Nissan Rogue or similar', '5', '4', 'IFAR', '70.00', '88.30')
+# Test with [(1731192271, 'Alamo', 'Midsize SUV', 'Nissan Rogue or similar', '5', '4', 'IFAR', '2024-11-09', 6, '2024-11-10', 0, 1, '70.00', '88.30', True),]
 
 def dtm_update(test: bool, hints_enabled: bool, query: list) -> None:
     """
     Compares and updates alamo_dtm.json with alamo classes.
 
-    Args:
+    Parameters:
         test (bool)
         hints_enabled (bool)
         query (list)
@@ -134,33 +134,68 @@ def dtm_update(test: bool, hints_enabled: bool, query: list) -> None:
     alt = 0
     # Compare dtms and descriptions
     for i in query:
-        # if type Premium (since there are 2 of them)
+            # if type Premium (since there are 2 of them)
         if i[2] == "Premium":
+            print("Premium was passed")
             pass
-        # everything else
-        elif i[2] in dtm_import_list:
-            test and print(dtm_import_list[i[2]])
-            if i[3] != dtm_import_list[i[2]]["description"]:
-                dtm_import_list[i[2]]["description"] = i[3]
-                logging.info(f'HINT {__name__}: "{i[2]}" had their description assigned to "{i[3]}".')
-                alt += 1
-            if i[6] != dtm_import_list[i[2]]["dtm"]:
-                dtm_import_list[i[2]]["dtm"] = i[6]
-                logging.info(f'HINT {__name__}: "{i[2]}" had their dtm assigned to "{i[6]}".')
-                alt += 1
         else:
-            print(f'we found something new: {i[2]}')
+            for category, vehtype in dtm_import_list.items():
+                if i[2] in vehtype:
+                    # everything else
+                    test and print(vehtype[i[2]])
+                    if i[3] != vehtype[i[2]]["description"]:
+                        vehtype[i[2]]["description"] = i[3]
+                        logging.info(f'HINT {__name__}: "{i[2]}" had their description assigned to "{i[3]}".')
+                        alt += 1
+                    if i[6] != vehtype[i[2]]["dtm"]:
+                        vehtype[i[2]]["dtm"] = i[6]
+                        logging.info(f'HINT {__name__}: "{i[2]}" had their dtm assigned to "{i[6]}".')
+                        alt += 1
+                    break
+            else:
+                print(f'we found something new: {i[2]}')
     
     if alt == 0:
         hints_enabled and print(f"HINT {__name__}: dtm's in alamo_dtm.json are up to date.")
     # write to json file
     else:
-        with open("functions_alamo/alamo_dtm.json", "w") as f:
-            json.dump(dtm_import_list, f, indent=4)        
+        print('something changed in the file')
+        # with open("functions_alamo/alamo_dtm.json", "w") as f:
+        #     json.dump(dtm_import_list, f, indent=4)        
 
+
+def createarr():
+    queries = [(1731192271, 'Alamo', 'Midsize SUV', 'Nissan Rogue or similar', '5', '4', 'IFAR', '2024-11-09', 6, '2024-11-10', 0, 1, '70.00', '88.30', True),
+(1731192271, 'Alamo', 'Compact SUV', 'Hyundai Kona or similar', '5', '3', 'CFAR', '2024-11-09', 6, '2024-11-10', 0, 1, '64.00', '80.38', True),
+(1731192271, 'Alamo', 'Economy', 'Mitsubishi Mirage or similar', '4', '2', 'ECAR', '2024-11-09', 6, '2024-11-10', 0, 1, '70.42', '87.81', True),
+(1731192271, 'Alamo', 'Full Size', 'Toyota Camry or Chevrolet Malibu or similar', '5', '4', 'FCAR', '2024-11-09', 6, '2024-11-10', 0, 1, '70.24', '88.93', True),
+(1731192271, 'Alamo', 'Standard Pickup', 'Toyota Tacoma or similar', '4', '3', 'SPAR', '2024-11-09', 6, '2024-11-10', 0, 1, '70.00', '89.58', True),
+(1731192271, 'Alamo', 'Midsize', 'Toyota Corolla or similar', '5', '3', 'ICAR', '2024-11-09', 6, '2024-11-10', 0, 1, '72.64', '91.05', True),
+(1731192271, 'Alamo', 'Standard', 'VW Jetta or similar', '5', '3', 'SCAR', '2024-11-09', 6, '2024-11-10', 0, 1, '73.62', '92.22', True),
+(1731192271, 'Alamo', 'Pickup', 'Ford F150 or similar', '4', '4', 'PPAR', '2024-11-09', 6, '2024-11-10', 0, 1, '72.78', '93.38', True),
+(1731192271, 'Alamo', 'Compact', 'Nissan Versa or similar', '5', '2', 'CCAR', '2024-11-09', 6, '2024-11-10', 0, 1, '74.97', '93.51', True),
+(1731192271, 'Alamo', 'Premium', 'Nissan Maxima or similar', '5', '4', 'PCAR', '2024-11-09', 6, '2024-11-10', 0, 1, '74.30', '93.63', True),
+(1731192271, 'Alamo', 'Standard SUV', 'Chevrolet Equinox or similar', '5', '5', 'SFAR', '2024-11-09', 6, '2024-11-10', 0, 1, '75.00', '94.79', True),
+(1731192271, 'Alamo', 'Full Size Elite', 'Audi A5 Sportback or similar', '5', '3', 'GXAR', '2024-11-09', 6, '2024-11-10', 0, 1, '75.94', '96.43', True),
+(1731192271, 'Alamo', 'Standard Elite', 'BMW 228i Gran Coupe or similar', '5', '2', 'RXAR', '2024-11-09', 6, '2024-11-10', 0, 1, '78.00', '98.30', True),
+(1731192271, 'Alamo', 'Premium Special', 'Audi A3 or BMW 2 Series Gran Coupe or similar', '5', '3', 'PXAR', '2024-11-09', 6, '2024-11-10', 0, 1, '78.32', '98.31', True),
+(1731192271, 'Alamo', 'Standard Elite SUV', 'Audi Q3, Cadillac XT4 or similar', '5', '3', 'RFAR', '2024-11-09', 6, '2024-11-10', 0, 1, '77.97', '98.31', True),
+(1731192271, 'Alamo', 'Luxury', 'Audi A4 or BMW 3 Series or similar', '5', '4', 'LCAR', '2024-11-09', 6, '2024-11-10', 0, 1, '83.96', '105.64', True),
+(1731192271, 'Alamo', 'Premium Crossover', 'Dodge Durango or similar', '7', '3', 'PGAR', '2024-11-09', 6, '2024-11-10', 0, 1, '84.00', '106.07', True),
+(1731192271, 'Alamo', 'Jeep', 'Jeep Wrangler 2 Door or similar', '4', '3', 'IJAR', '2024-11-09', 6, '2024-11-10', 0, 1, '104.59', '131.06', True),
+(1731192271, 'Alamo', '7 Passenger Minivan', 'Chrysler Pacifica or similar', '7', '5', 'MVAR', '2024-11-09', 6, '2024-11-10', 0, 1, '107.98', '135.33', True),
+(1731192271, 'Alamo', 'Jeep Wrangler 4 door', 'Jeep Wrangler Unlimited or similar', '5', '5', 'FJAR', '2024-11-09', 6, '2024-11-10', 0, 1, '109.87', '137.96', True),
+(1731192271, 'Alamo', 'You Click We Pick', 'Vehicle determined upon pick-up, Compact or Larger', None, None, 'XXAR', '2024-11-09', 6, '2024-11-10', 0, 1, '117.99', '147.58', True),
+(1731192271, 'Alamo', 'Premium SUV', 'Ford Expedition Max, Jeep Wagoneer L or similar', '8', '7', 'PFAR', '2024-11-09', 6, '2024-11-10', 0, 1, '119.00', '149.79', True),
+(1731192271, 'Alamo', 'Premium', 'Chrysler 300 or similar', '5', '4', 'PDAR', '2024-11-09', 6, '2024-11-10', 0, 1, '259.98', '322.15', True),
+(1731192271, 'Alamo', 'Midsize Luxury SUV', 'Mercedes GLE, BMW X5 or similar', '5', '4', 'UDAR', '2024-11-09', 6, '2024-11-10', 0, 1, '299.00', '371.87', True),
+]
+    return queries
 
 if __name__ == "__main__":
     test = False
     hints_enabled = True
-    myvar = example_tuples.example_tuples
-    dtm_update(test, hints_enabled, myvar)
+    # myvar = example_tuples.example_tuples
+    # dtm_update(test, hints_enabled, myvar)
+    testarr = createarr()
+    dtm_update(test, hints_enabled, testarr)
--- server_mirror/functions/alamo_dtm.py
+++ server_mirror/functions/alamo_dtm.py
@@ -1,10 +1,10 @@
-# Server-Mirror
+ # Server-Mirror
 
 import json
 
 from functions import import_logging
 
-dtm_logger = import_logging.main("../resources/alamo_dtm_logs.txt", "dtm_logger")
+dtm_logger = import_logging.main("resources/alamo_dtm_logs.txt", "dtm_logger")
 
 
 def dtm_update(test: bool, hints_enabled: bool, query: list) -> None:
@@ -14,14 +14,14 @@ def dtm_update(test: bool, hints_enabled: bool, query: list) -> None:
     Parameters:
         test (bool): No application yet.
         hints_enabled (bool):
-        query (list): ['Midsize Luxury SUV', 'Mercedes GLE, BMW X5 or similar', '5', '4', 'UDAR', '2024-11-09', 6, '2024-11-10', 0, 1, '299.00', '371.87', True), ...,]
+        query (list): [(1731192271, 'Alamo', 'Midsize SUV', 'Nissan Rogue or similar', '5', '4', 'IFAR', '2024-11-09', 6, '2024-11-10', 0, 1, '70.00', '88.30', True), ...,]
 
     Returns:
         None:
     """
 
     # Load alamo dtm's
-    with open("../resources/alamo_dtm.json", "r") as file:
+    with open("resources/alamo_dtm.json", "r") as file:
         dtm_import_list = json.load(file)
 
     # Counter for any changes made for json file.
@@ -31,26 +31,31 @@ def dtm_update(test: bool, hints_enabled: bool, query: list) -> None:
         # if type Premium (since there are 2 of them)
         if i[2] == "Premium":
             pass
-        # everything else
-        elif i[2] in dtm_import_list:
-            test and print(dtm_import_list[i[2]])
-            if i[3] != dtm_import_list[i[2]]["description"]:
-                dtm_import_list[i[2]]["description"] = i[3]
-                dtm_logger.info(f'HINT {__name__}: "{i[2]}" had their description assigned to "{i[3]}".')
-                alt += 1
-            if i[6] != dtm_import_list[i[2]]["dtm"]:
-                dtm_import_list[i[2]]["dtm"] = i[6]
-                dtm_logger.info(f'HINT {__name__}: "{i[2]}" had their dtm assigned to "{i[6]}".')
-                alt += 1
         else:
-            print(f'we found something new: {i[2]}')
+            for category, vehtype in dtm_import_list.items():
+                if i[2] in vehtype:
+                    # everything else
+                    if i[3] != vehtype[i[2]]["description"]:
+                        vehtype[i[2]]["description"] = i[3]
+                        dtm_logger.info(f'HINT {__name__}: "{i[2]}" had their description assigned to "{i[3]}".')
+                        alt += 1
+                    if i[6] != vehtype[i[2]]["dtm"]:
+                        vehtype[i[2]]["dtm"] = i[6]
+                        dtm_logger.info(f'HINT {__name__}: "{i[2]}" had their dtm assigned to "{i[6]}".')
+                        alt += 1
+                    break
+            else:
+                print(f'we found something new: {i[2]}')
+                dtm_logger.warning(f'{__name__}: we found something new: {i[2]}')
 
     if alt == 0:
         hints_enabled and print(f"HINT {__name__}: dtm's in alamo_dtm.json are up to date.")
+        dtm_logger.info("dtm's in alamo_dtm.json are up to date.")
     # write to json file
     else:
-        with open("../resources/alamo_dtm.json", "w") as f:
+        with open("resources/alamo_dtm.json", "w") as f:
             json.dump(dtm_import_list, f, indent=4)
+        dtm_logger.info(f"{__name__}: Changes were saved.")
 
 
 def logging_test() -> None:
