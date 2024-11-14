1380b2fa0a331014fbcc9c7d0061c7adc4d02cdb
Jeremy Desktop <jeremycrisostomo@gmail.com>
2024-11-13 16:44:53 -0800

    working database update with location in functions_gen/ #2

--- functions_gen/create_db.py
+++ functions_gen/create_db.py
@@ -21,6 +21,7 @@ def create_database(test: bool, hints_enabled: bool) -> None:
     CREATE TABLE IF NOT EXISTS rental_prices (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         epoch_ident INT NOT NULL,
+        location TEXT NOT NULL,
         service TEXT NOT NULL,
         type TEXT NOT NULL,
         model TEXT NOT NULL,
@@ -42,6 +43,7 @@ def create_database(test: bool, hints_enabled: bool) -> None:
     CREATE TABLE IF NOT EXISTS cheapest_prices(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         epoch_ident INT NOT NULL,
+        location TEXT NOT NULL,
         service TEXT NOT NULL,
         type TEXT NOT NULL,
         model TEXT NOT NULL,
@@ -75,7 +77,7 @@ def db_update(test: bool, hints_enabled: bool, option_tuples_cleaned: list) -> N
         option = (*option[:-1], int(option[-1])) # rebuilds tuple with converted boolean into integer
 
         cursor.execute('''
-        INSERT INTO rental_prices (epoch_ident, service, type, model, pax, lug, data_dtm_track, date_scr_date, date_scr_int, date_rsv_date, date_rsv_int, adv_rsv, daily, total, unlimited)
+        INSERT INTO rental_prices (epoch_ident, location, service, type, model, pax, lug, data_dtm_track, date_scr_date, date_scr_int, date_rsv_date, date_rsv_int, adv_rsv, daily, total, unlimited)
         Values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
         ''', option)
 
@@ -110,7 +112,7 @@ def dl_from_downloaded(test: bool, hints_enabled: bool):
     local_conn = sqlite3.connect("rental_data.db")
     local_cursor = local_conn.cursor()
 
-    downloaded_conn = sqlite3.connect("downloadeD_data.db")
+    downloaded_conn = sqlite3.connect("downloaded_data.db")
     downloaded_cursor = downloaded_conn.cursor()
 
     tgt_table = "rental_prices"
@@ -121,6 +123,7 @@ def dl_from_downloaded(test: bool, hints_enabled: bool):
     CREATE TABLE IF NOT EXISTS {tgt_table} (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         epoch_ident INT NOT NULL,
+        location TEXT NOT NULL,
         service TEXT NOT NULL,
         type TEXT NOT NULL,
         model TEXT NOT NULL,
@@ -152,7 +155,51 @@ def dl_from_downloaded(test: bool, hints_enabled: bool):
     local_conn.close()
     downloaded_conn.close()
 
-    db_export_rental_prices(test, hints_enabled, "Alamo")
+    db_export_rental_prices(False, hints_enabled, "Alamo")
+
+
+def add_location():
+    db_path = 'rental_data.db'
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+    print("adding column at [2]")
+    # cursor.execute("ALTER TABLE rental_prices RENAME TO old_prices;")
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS rental_prices (
+            id INTEGER NOT NULL,
+            epoch_ident INT NOT NULL,
+            location TEXT NOT NULL,
+            service TEXT NOT NULL,
+            type TEXT NOT NULL,
+            model TEXT NOT NULL,
+            pax INTEGER,
+            lug INTEGER,
+            data_dtm_track TEXT NOT NULL,
+            date_scr_date TEXT NOT NULL,
+            date_scr_int INTEGER NOT NULL,
+            date_rsv_date TEXT NOT NULL,
+            date_rsv_int INTEGER NOT NULL,
+            adv_rsv INTEGER NOT NULL,
+            daily REAL NOT NUll,
+            total REAL NOT NULL,
+            unlimited INTEGER NOT NULL
+        );
+    """)
+
+    cursor.execute("""
+        INSERT INTO rental_prices (id, epoch_ident, location, service, type, model, pax, lug, data_dtm_track, date_scr_date, date_scr_int, date_rsv_date, date_rsv_int, adv_rsv, daily, total, unlimited)
+        SELECT id, epoch_ident, "SNA", service, type, model, pax, lug, data_dtm_track, date_scr_date, date_scr_int, date_rsv_date, date_rsv_int, adv_rsv, daily, total, unlimited
+        FROM old_prices;
+    """)
+
+    cursor.execute("DROP TABLE old_prices;")
+
+    conn.commit()
+    conn.close()
+    print('location added')
+
+    db_export_rental_prices(False, hints_enabled, "Alamo")
 
 
 if __name__ == "__main__":
@@ -170,8 +217,12 @@ if __name__ == "__main__":
             break
         elif choice == "3":
             dl_from_downloaded(test, hints_enabled)
+            break
+        elif choice == "location":
+            add_location()
+            break
         else:
-            print(f'{choice} was an invalid choice.\n')
+            print(f'{choice} was an invalid choice. Enter again.')
 
 
 """
